41d3755bb9e2d0a5f21e2f1af8332bfeb8318d84
adriancm <adrian.cepillo@i4s.com>
2016-06-21 20:04:47 +0200

    Slim installed and configured

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module Twitter
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+  	  g.orm             :active_record
+  	  g.template_engine :slim
+  	  g.test_framework  :test_unit, fixture: false
+  	  g.stylesheets     true
+      g.javascripts     true
+    end
   end
 end
