ce87d091d03b12f007c9132d93e52c1185781bfb
Adrian Cepillo <adrian.cepillo@gmail.com>
2016-07-12 12:33:50 +0200

    Set as helper method current_user

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::BaseController < ApplicationController
 
   before_action :doorkeeper_authorize!
 
+  helper_method :current_user
+
   def current_user
     @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
