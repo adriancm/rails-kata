cc46b2d61ea2d8b1bbecde0d19a6699be01a10b0
Adrian Cepillo <adrian.cepillo@gmail.com>
2016-07-14 11:22:30 +0200

    Added some validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
 
+  validates :message, presence: true, length: { maximum: 150 }
+
   scope :lasts, ->{ order(created_at: :desc) }
 
 end
--- db/migrate/20160712133118_create_tweets.rb
+++ db/migrate/20160712133118_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
       t.belongs_to :user, index: true, foreign_key: true
-      t.string :message
+      t.string :message, limit: 150, null: false
 
       t.timestamps null: false
     end
